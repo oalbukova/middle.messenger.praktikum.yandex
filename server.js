cafcea6c581b57a724f579b7afbf48c195a19b79
oalbukova <o.albukova@gmail.com>
2022-12-31 00:52:54 +0300

    fix readme

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static('dist'));
 
-app.get('/*', (request, response) => {
-  response.sendFile(`${__dirname}/dist/index.html`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
 app.listen(PORT, () => {
